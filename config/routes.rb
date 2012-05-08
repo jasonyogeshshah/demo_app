a304b9c12de07adfc3678268fb910c0a96b78bce
jasonshah <jasonshah@Post.harvard.edu>
2012-05-07 22:33:52 -0700

    13 commit

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ DemoApp::Application.routes.draw do
 
 # root :to => redirect("app/views/users/index.html.erb")
 
- root_url  => 'public/500.html' 
+# root_url  => 'public/500.html' 
 
 # root_url  => '../app/views/users/index.html.erb'
 
@@ -59,6 +59,8 @@ DemoApp::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+
+	root :to => 'public/500.html'
 
   # See how all your routes lay out with "rake routes"
 
