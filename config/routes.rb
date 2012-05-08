efecbe0027794230dc440c2e37226cde56eba7ea
jasonshah <jasonshah@Post.harvard.edu>
2012-05-07 22:12:26 -0700

    8 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 DemoApp::Application.routes.draw do
 #  resources :users,
 
+root :to => redirect("../app/views/users/index.html.erb")
 
-root_url  => '../app/views/users/index.html.erb',
+# root_url  => '../app/views/users/index.html.erb'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
